b200af6e99fd4b6cd123e60d45090bed6d026776
aswinrprasad <aswinrprasad@gmail.com>
2022-03-06 14:24:07 +0530

    Eureka Server Initial Commit

---
+++ src/test/java/com/aswin/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aswin.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
